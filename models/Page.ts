0b0149518836e78078ae8d014d1de48f21ae4dc4
Lvov Nikita <lvov.nikita@yahoo.com>
2022-06-30 13:21:20 +0300

    Add Page model's field options

--- models/Page.ts
+++ models/Page.ts
@@ -1,9 +1,34 @@
 import { relationship, text } from '@keystone-6/core/fields'
 
+interface pageListOptions {
+    title,
+    menuItemRef,
+    blocks,
+}
+
+const opts: pageListOptions = {
+    title: {
+        label: 'Заголовок страницы',
+        validation: {
+            isRequired: true
+        }
+    },
+    menuItemRef: {
+        label: 'Переход на страницу через пункт меню:',
+        ref: 'MenuItem.pageRef',
+        many: false
+    },
+    blocks: {
+        label: 'Блоки, составляющие страницу',
+        ref: 'Block',
+        many: true
+    }
+}
+
 module.exports = {
     fields: {
-        title: text(),
-        menuItemRef: relationship({ ref: 'MenuItem.pageRef', many: false }),
-        blocks: relationship({ ref: 'Block', many: true })
+        title: text(opts.title),
+        menuItemRef: relationship(opts.menuItemRef),
+        blocks: relationship(opts.blocks)
     }
 }
