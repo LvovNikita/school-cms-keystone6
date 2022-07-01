4c4bd958f48d0a1158c1a5b5ec83a7cfc5c4b0da
Lvov Nikita <lvov.nikita@yahoo.com>
2022-07-01 14:36:26 +0300

    Add process.env.DATABASE_URL variable to database config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import { initData } from '../data/init'
 
 module.exports = {
     provider: 'sqlite',
-    url: 'file:./keystone.db',
+    url: process.env.DATABASE_URL || 'file:./keystone.db',
     async onConnect (context) {
         if (process.argv.includes('--init-data')) {
             await initData(context)
